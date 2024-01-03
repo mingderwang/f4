b3baca63ec4bbf500cdcc0997b85f9809777589a
Ming-der Wang <mingderwang@gmail.com>
2024-01-03 23:00:00 +0800

    add deployment with forge

--- src/MyToken.sol
+++ src/MyToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
-    constructor(address initialOwner) ERC20("MyToken", "MTK") Ownable(initialOwner) {}
+    constructor(address initialOwner) ERC20("DaSaBit", "DASA") Ownable(initialOwner) {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
